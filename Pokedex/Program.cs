98b123cf5616be90670ae408d7834d380edfb9ae
JOSE EDUARDO PIRES FARAH <jose.farah@cs.up.edu.br>
2025-04-22 09:59:05 -0300

    Att01

---
+++ Pokedex/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using Pokedex;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=pokemons.db"));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.MapGet("/pokemons", (AppDbContext db) => 
+{
+    var listaPokemons = db.pokemons.ToList();
+    return Results.Ok(listaPokemons);
+});
+
+app.MapGet("/pokemons/{id}", async (int id, AppDbContext db) => 
+{
+    var pokemon = await db.pokemons.FindAsync(id);
+    return pokemon is not null ? Results.Ok(pokemon) : Results.NotFound("Pokemon não encontrado!");
+});
+
+app.MapPost("/pokemons", async (AppDbContext db, Pokemon pokemon) => 
+{
+    db.pokemons.Add(pokemon);
+    await db.SaveChangesAsync();
+    return Results.Created($"pokemons/{pokemon.Id}", pokemon);
+});
+
+app.Run();
